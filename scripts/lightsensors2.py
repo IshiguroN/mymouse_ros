4acd4b17f9a3ff7a65f8dda8638a9056eb0be659
Ishiguro Naoya <400diehard@gmail.com>
2021-03-18 01:08:27 +0900

    new files

--- scripts/lightsensors2.py
+++ scripts/lightsensors2.py
@@ -2,25 +2,12 @@
 import sys, rospy
 from mymouse_ros.msg import MyLightSensorValues
 
-def get_freq():
-    f = rospy.get_param('lightsensors_freq',20)
-    try:
-        if f <= 0.0:
-            raise Exception()
-    except:
-        rospy.logerr("value error: lightsensors_freq")
-        sys.exit(1)
-
-    return f
-
-
 if __name__ == '__main__':
     devfile = '/dev/rtlightsensor0'
     rospy.init_node('lightsensors')
     pub = rospy.Publisher('lightsensors',MyLightSensorValues, queue_size=1)
-    
-    freq = get_freq()
-    rate = rospy.Rate(freq)
+
+    rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         try:
             with open(devfile,'r') as f:
@@ -38,10 +25,4 @@ if __name__ == '__main__':
 
 	except I0Error:
 	    rospy.logerr("cannot write to " + devfile)
-        f = get_freq()
-        if f != freq:
-            freq = f
-            rate = rospy.Rate(freq)
-
-
 	rate.sleep()
